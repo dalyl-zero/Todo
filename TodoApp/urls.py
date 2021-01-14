fa0f51dc3e4855fb5b716534620506c119395e08
dalyl <adidadi.adiden@gmail.com>
2021-01-14 16:22:05 +0100

    Can update task

--- TodoApp/urls.py
+++ TodoApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexView
+from .views import *
 
 urlpatterns = [
-    path('', IndexView.as_view())
+    path('', IndexView.as_view(), name='app'),
+    path('update/<int:id>', UpdateView.as_view(), name='update')
 ]
--- TodoApp/views.py
+++ TodoApp/views.py
@@ -3,7 +3,6 @@ from django.views import View
 from .models import Task
 from .forms import TaskForm
 
-
 class IndexView(View):
     def get(self, request):
         tasks = Task.objects.all()
@@ -15,6 +14,25 @@ class IndexView(View):
 
     def post(self, request):
         form = TaskForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+        return redirect('/')
+
+
+class UpdateView(View):
+    def get(self, request, id):
+        task = Task.objects.get(id=id)
+        form = TaskForm(instance=task)
+
+        context = {'form':form}
+
+        return render(request, 'TodoApp/update.html', context)
+
+    def post(self, request, id):
+        task = Task.objects.get(id=id)
+        form = TaskForm(request.POST, instance=task)
 
         if form.is_valid():
             form.save()
